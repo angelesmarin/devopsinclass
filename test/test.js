fee7ee2ba02b3a0011263d7110d2b0261dd4c17b
Angeles Marin <angeles.marinbatana@bsu.edu>
2024-11-26 10:04:18 -0500

    Update test.js

--- test/test.js
+++ test/test.js
@@ -9,6 +9,13 @@ describe('Regular factorial value', function() {
     it('should return 1 for factorial(1)', function() {
         assert.strictEqual(main.factorial(1), 1); 
     });
+    it('should return 120 for factorial of 5', function() {
+        assert.strictEqual(main.factorial(5), 120);
+    });
+
+    it('should return 3628800 for factorial of 10', function() {
+        assert.strictEqual(main.factorial(7), 3628800);
+    });
 });
 describe('negative factorial value', function() {
     it('should return undefined for factorial(-1)', function() {
